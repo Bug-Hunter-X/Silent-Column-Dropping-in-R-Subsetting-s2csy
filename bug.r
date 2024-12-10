4356aa2a736696b812f93bae637b3e3e5cb14dee
BugHunter-X <askzenai@gmail.com>
2024-12-10 18:54:05 +0100

    Add bug.r file

---
+++ bug.r
@@ -0,0 +1,7 @@
+```r
+# This code attempts to subset a data frame using a character vector that contains a column name that doesn't exist.
+
+df <- data.frame(A = 1:3, B = 4:6)
+cols_to_select <- c("A", "C")
+subset_df <- df[, cols_to_select]
+```
